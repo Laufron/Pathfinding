1bb82aa89d59864e47ab2a691796e62cfb5c294c
Laufron <50317366+Laufron@users.noreply.github.com>
2025-11-06 15:55:16 +0100

    Correction sur Dijkstra et A* pour s'assurer qu'aucune direction n'est privilégiée en cas d'égalité

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,7 @@ from collections import deque
 from collections.abc import Generator
 from enum import Enum, auto
 from heapq import heappop, heappush
+from itertools import count
 
 from grid import CellIndex, CellType, Grid
 
@@ -51,12 +52,14 @@ def bfs(
 def dijkstra(
     grid: Grid, start: CellIndex, goal: CellIndex
 ) -> Generator[tuple[CellIndex, CellDynState]]:
+    counter = count()
     min_distances: dict[CellIndex, float] = {start: 0}
-    min_heap = [(min_distances[start], start)]
     parents = dict()
 
+    min_heap = [(min_distances[start], next(counter), start)]
+
     while min_heap:
-        current_distance, current = heappop(min_heap)
+        current_distance, _, current = heappop(min_heap)
         if current_distance > min_distances[current]:
             # Cas ou on a trouvé un chemin plus court vers la cellule entre temps : on ignore le chemin long
             continue
@@ -78,7 +81,7 @@ def dijkstra(
             ):
                 min_distances[neighbour] = new_neighbour_distance
                 parents[neighbour] = current
-                heappush(min_heap, (new_neighbour_distance, neighbour))
+                heappush(min_heap, (new_neighbour_distance, next(counter), neighbour))
                 yield neighbour, CellDynState.QUEUED
 
     if goal in parents:
@@ -99,12 +102,14 @@ def A_star(
     # g: cout reel
     # h: heuristique
     # f = g + h
+    counter = count()
     min_gscore: dict[CellIndex, float] = {start: 0}
-    min_heap = [(min_gscore[start] + h(start, goal), start)]
     parents = dict()
 
+    min_heap = [(min_gscore[start] + h(start, goal), next(counter), start)]
+
     while min_heap:
-        current_fscore, current = heappop(min_heap)
+        current_fscore, _, current = heappop(min_heap)
         if current_fscore - h(current, goal) > min_gscore[current]:
             # Cas ou on a trouvé un chemin plus court vers la cellule entre temps : on ignore le chemin long
             continue
@@ -123,7 +128,10 @@ def A_star(
             if neighbour not in min_gscore or neighbour_gscore < min_gscore[neighbour]:
                 min_gscore[neighbour] = neighbour_gscore
                 parents[neighbour] = current
-                heappush(min_heap, (neighbour_gscore + h(neighbour, goal), neighbour))
+                heappush(
+                    min_heap,
+                    (neighbour_gscore + h(neighbour, goal), next(counter), neighbour),
+                )
                 yield neighbour, CellDynState.QUEUED
 
     if goal in parents:
